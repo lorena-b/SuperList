ebb770c2d617926256456824ce6f392f8686f6c6
dannychn11 <67206771+dannychn11@users.noreply.github.com>
2021-01-16 15:01:53 -0500

    changed html

--- client/authorization_code/app.js
+++ client/authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '03ff3d8c083a46af9727927f58211eae'; // Your client id
+var client_secret = '969305ea7a924d70941962a3cbc4dead'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
